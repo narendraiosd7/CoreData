39df1611061641317d24e8895df34cff5abe61e6
nvadde <nvadde@uworld.in>
2023-05-03 07:28:02 +0530

    core data model creation

---
+++ MasteringCoreData/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MasteringCoreData
+//
+//  Created by UW-IN-LPT0108 on 5/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MasteringCoreData/MasteringCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringCoreDataApp.swift
+//  MasteringCoreData
+//
+//  Created by UW-IN-LPT0108 on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MasteringCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
